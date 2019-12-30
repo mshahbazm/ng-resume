b8d9a5e73f2937586eae4ff969218255085b0520
mshahbazm <msmanzoor91@gmail.com>
2019-12-30 22:56:07 +0500

    *Created required components *Created default theme component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ThemeComponent } from './theme/theme.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {ProjectsListingComponent} from "./projects-listing/projects-listing.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ThemeComponent},
+  {path: 'projects', component: ProjectsListingComponent},
+  {path: '404', component: NotFoundComponent},
+  {path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuickViewComponent } from './quick-view/quick-view.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { EducationComponent } from './education/education.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { ThemeComponent } from './theme/theme.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProjectsListingComponent } from './projects-listing/projects-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuickViewComponent,
+    AboutComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ProjectsComponent,
+    TestimonialsComponent,
+    EducationComponent,
+    CertificationsComponent,
+    ThemeComponent,
+    NotFoundComponent,
+    ProjectsListingComponent
   ],
   imports: [
     BrowserModule,
